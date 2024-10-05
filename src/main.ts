452db73dff024160fb71ceecaaebf46416e062f7
Caleb Brose <5447118+cmbrose@users.noreply.github.com>
2024-10-05 01:51:48 +0000

    Add option to just write to stdin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ async function main(): Promise<void> {
     const session = await connectSession();
     const channel = await session.openChannel();
 
-    channel.onDataReceived(e => console.log(`STDOUT: ${e.toString("utf-8")}`));
+    channel.onDataReceived(e => {
+        console.log(`STDOUT: ${e.toString("utf-8")}`)
+    });
     
     channel.onExtendedDataReceived(e => {
         if (e.dataTypeCode !== SshExtendedDataType.STDERR) {
@@ -18,17 +20,20 @@ async function main(): Promise<void> {
         console.log(`STDERR: ${e.toString()}`);
     });
 
-    channel.onClosed(e => console.log(`CLOSED: ${JSON.stringify(e)}`));
+    channel.onClosed(e => {
+        console.log(`CLOSED: ${JSON.stringify(e)}`)
+        process.exit(0)
+    });
 
     const execRequestMessage = new CommandRequestMessage();
     execRequestMessage.command = commandToRun;
     channel.request(execRequestMessage);
 
-    console.log('Command sent. Use the following options to interact (remember to hit return!)');
-    console.log(' - i => SIGINT');
-    console.log(' - t => SIGTERM');
-    console.log(' - k => SIGKILL');
-    console.log(' - q => close and exit');
+    console.log('Command sent. You can now type to write to stdin or use the following commands - remember to hit return!');
+    console.log(' - !i => SIGINT');
+    console.log(' - !t => SIGTERM');
+    console.log(' - !k => SIGKILL');
+    console.log(' - !q => close and exit');
     console.log('');
 
     const sendSignal = async (signal: 'INT' | 'TERM' | 'KILL') => {
@@ -41,19 +46,22 @@ async function main(): Promise<void> {
     process.stdin.addListener("data", async function(d) {
         const c = d.toString().trim();
 
-        if (c === 'q') {
+        if (c === '!q') {
             console.log("closing and exiting");
             await channel.close();
             process.exit(0)
-        } else if (c === 'i') {
+        } else if (c === '!i') {
             console.log("sending sigint");
             await sendSignal('INT');
-        } else if (c === 't') {
+        } else if (c === '!t') {
             console.log("sending sigterm");
             await sendSignal('TERM');
-        } else if (c === 'k') {
+        } else if (c === '!k') {
             console.log("sending sigkill");
             await sendSignal('KILL');
+        } else {
+            // Writing to stdin
+            await channel.send(Buffer.from(c))
         }
     });
 }
